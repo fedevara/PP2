550ecb71bfd0df4801c1ba9e5636ba4a554e89c9
fedevara <emanuelvara@hotmail.com>
2017-04-15 00:17:26 -0300

    Agregado establecimientos

--- app/src/main/java/project/pack/facade/Facade.java
+++ app/src/main/java/project/pack/facade/Facade.java
@@ -2,9 +2,11 @@ package project.pack.facade;
 
 import java.util.Date;
 
+import project.pack.controller.ManejoEstablecimiento;
 import project.pack.controller.ManejoIncidente;
 import project.pack.domain.Categoria;
 import project.pack.domain.Coordenada;
+import project.pack.domain.Establecimiento;
 import project.pack.domain.Incidente;
 
 /**
@@ -14,11 +16,11 @@ import project.pack.domain.Incidente;
 public class Facade {
 
     private ManejoIncidente manejoIncidente;
+    private ManejoEstablecimiento manejoEstablecimiento;
 
     public Facade() {
-
         manejoIncidente = new ManejoIncidente();
-
+        manejoEstablecimiento = new ManejoEstablecimiento();
     }
 
     public Incidente crearIncidente(String titulo, String descripcion, Date fecha, Categoria categoria, Coordenada lugar){
@@ -29,5 +31,10 @@ public class Facade {
 
     public Incidente obtenerIncidente(String id1) {
         return manejoIncidente.getIncidente(id1);
+    }
+
+    public void crearEstablecimiento(String nombre, Categoria categoria, Coordenada lugar) {
+        Establecimiento establecimiento = manejoEstablecimiento.crearEstacimiento(nombre, categoria, lugar);
+        manejoEstablecimiento.guardarEstablecimiento(establecimiento);
     }
 }
