69dbcc2406fa867597e09bc8ddaa3438120f3acd
LucasTolosa <tolosa.lmt@gmail.com>
2017-07-06 23:24:40 -0300

    Correccion simple

---
+++ app/src/test/java/project/pack/persistence/PersistenciaEstablecimientoTest.java
@@ -0,0 +1,43 @@
+package project.pack.persistence;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by lukas on 06/07/2017.
+ */
+public class PersistenciaEstablecimientoTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void getInstance() throws Exception {
+
+    }
+
+    @Test
+    public void addEstablecimiento() throws Exception {
+
+    }
+
+    @Test
+    public void getListaEstablecimiento() throws Exception {
+
+    }
+
+    @Test
+    public void vaciarBD() throws Exception {
+
+    }
+
+}
